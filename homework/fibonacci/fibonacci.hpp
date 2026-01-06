8182a2a28f7979ef4825d0a3ffb54ced84b63259
Sebastian Sosnowski <mefiu672@gmail.com>
2026-01-06 17:10:09 +0100

    Add iterative implementation

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -2,7 +2,18 @@
 
 int fibonacci_iterative(int sequence) {
     // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0 or sequence == 1)
+        return sequence;
+    if (sequence < 0)
+        return -1;
+
+    int curr{0}, prev_prev{0}, prev{1};
+    for (int i = 2; i <= sequence; i++) {
+        curr = prev_prev + prev;
+        prev_prev = prev;
+        prev = curr;
+    }
+    return curr;
 }
 
 int fibonacci_recursive(int sequence) {
