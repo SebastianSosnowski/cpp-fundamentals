7fcd58990e51d5e25efe88f856a3965677c1f0b5
Sebastian Sosnowski <mefiu672@gmail.com>
2026-01-06 17:49:43 +0100

    Add NWD implementation

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,8 +1,25 @@
 #pragma once
+#include <numeric>
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (lhs == 0)
+        return rhs;
+    if (rhs == 0)
+        return lhs;
+    int a{lhs}, b{rhs}, quotient{}, reminder{};
+
+    do {
+        quotient = a / b;
+        reminder = a % b;
+        if (reminder == 0) {
+            break;
+        }
+        a = b;
+        b = reminder;
+    } while (reminder != 0);
+
+    return std::abs(b);
+    // or just return std::gcd(lhs, rhs); :)
 }
 
 int NWW(int lhs, int rhs) {
