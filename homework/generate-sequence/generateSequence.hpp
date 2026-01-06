077dd7e0a2647a880c8722f9af219c159a341579
Sebastian Sosnowski <mefiu672@gmail.com>
2026-01-06 18:48:06 +0100

    Add implementation of generateSequence

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -2,6 +2,14 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    std::vector<int> v;
+    if (count <= 0) {
+        return v;
+    }
+    v.reserve(count);
+
+    for (int i = 0, val = step; i < count; i++, val += step) {
+        v.push_back(val);
+    }
+    return v;
 }
