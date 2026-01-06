db421d9be561e5ffe0db4b26249c7a8b2a307bce
Sebastian Sosnowski <mefiu672@gmail.com>
2026-01-06 18:09:58 +0100

    Implement addEven function

--- homework/add-even/addEven.hpp
+++ homework/add-even/addEven.hpp
@@ -3,6 +3,12 @@
 
 int addEven(const std::vector<int>& numbers) {
     // TODO: Your implementation goes here
-    // Below return is only to make this function compile now
-    return -1;
+    int result{};
+
+    for (const auto& num : numbers) {
+        if (!(num % 2)) {
+            result += num;
+        }
+    }
+    return result;
 }
